b377f25d39408d988e0410102c1db133da33f516
Jiang Yin <jiangyin@gameframework.cn>
2017-04-24 03:12:49 +0800

    更新界面

--- Assets/GameMain/Scripts/UI/MenuForm.cs
+++ Assets/GameMain/Scripts/UI/MenuForm.cs
@@ -8,11 +8,15 @@ namespace StarForce
         [SerializeField]
         private Text m_Title = null;
 
+        [SerializeField]
+        private Text m_Description = null;
+
         protected internal override void OnOpen(object userData)
         {
             base.OnOpen(userData);
 
             m_Title.text = GameEntry.Localization.GetString("Game.Name");
+            m_Description.text = GameEntry.Localization.GetString("Game.Description");
         }
     }
 }
