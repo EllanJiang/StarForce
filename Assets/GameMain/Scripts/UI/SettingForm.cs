b3c7a67eec2d3ecbdba91e58eca9d8209b5fae44
Jiang Yin <jiangyin@gameframework.cn>
2017-05-02 01:34:26 +0800

    更新声音设置

--- Assets/GameMain/Scripts/UI/SettingForm.cs
+++ Assets/GameMain/Scripts/UI/SettingForm.cs
@@ -12,10 +12,28 @@ namespace StarForce
         private Text m_MusicText = null;
 
         [SerializeField]
+        private Toggle m_MusicMuteToggle = null;
+
+        [SerializeField]
+        private Slider m_MusicVolumeSlider = null;
+
+        [SerializeField]
         private Text m_SoundText = null;
 
         [SerializeField]
+        private Toggle m_SoundMuteToggle = null;
+
+        [SerializeField]
+        private Slider m_SoundVolumeSlider = null;
+
+        [SerializeField]
         private Text m_UISoundText = null;
+
+        [SerializeField]
+        private Toggle m_UISoundMuteToggle = null;
+
+        [SerializeField]
+        private Slider m_UISoundVolumeSlider = null;
 
         [SerializeField]
         private Text m_LanguageText = null;
@@ -41,6 +59,39 @@ namespace StarForce
         [SerializeField]
         private Text m_CancelText = null;
 
+        public void OnMusicMuteChanged(bool value)
+        {
+            GameEntry.Sound.Mute("Music", !value);
+            m_MusicVolumeSlider.gameObject.SetActive(value);
+        }
+
+        public void OnMusicVolumeChanged(float value)
+        {
+            GameEntry.Sound.SetVolume("Music", value);
+        }
+
+        public void OnSoundMuteChanged(bool value)
+        {
+            GameEntry.Sound.Mute("Sound", !value);
+            m_SoundVolumeSlider.gameObject.SetActive(value);
+        }
+
+        public void OnSoundVolumeChanged(float value)
+        {
+            GameEntry.Sound.SetVolume("Sound", value);
+        }
+
+        public void OnUISoundMuteChanged(bool value)
+        {
+            GameEntry.Sound.Mute("UISound", !value);
+            m_UISoundVolumeSlider.gameObject.SetActive(value);
+        }
+
+        public void OnUISoundVolumeChanged(float value)
+        {
+            GameEntry.Sound.SetVolume("UISound", value);
+        }
+
         public void OnSubmitButtonClick()
         {
             Close();
@@ -52,8 +103,14 @@ namespace StarForce
 
             m_TitleText.text = GameEntry.Localization.GetString("Setting.Title");
             m_MusicText.text = GameEntry.Localization.GetString("Setting.Music");
+            m_MusicMuteToggle.isOn = !GameEntry.Sound.IsMuted("Music");
+            m_MusicVolumeSlider.value = GameEntry.Sound.GetVolume("Music");
             m_SoundText.text = GameEntry.Localization.GetString("Setting.Sound");
+            m_SoundMuteToggle.isOn = !GameEntry.Sound.IsMuted("Sound");
+            m_SoundVolumeSlider.value = GameEntry.Sound.GetVolume("Sound");
             m_UISoundText.text = GameEntry.Localization.GetString("Setting.UISound");
+            m_UISoundMuteToggle.isOn = !GameEntry.Sound.IsMuted("UISound");
+            m_UISoundVolumeSlider.value = GameEntry.Sound.GetVolume("UISound");
             m_LanguageText.text = GameEntry.Localization.GetString("Setting.Language");
             m_LanguageTipsText.text = GameEntry.Localization.GetString("Setting.LanguageTips");
             m_EnglishText.text = GameEntry.Localization.GetString("Language.English");
