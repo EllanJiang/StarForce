f344b932f9d379c462c69c57bf97438c30ed88c0
Jiang Yin <jiangyin@gameframework.cn>
2017-05-03 02:43:21 +0800

    更新关于界面

--- Assets/GameMain/Scripts/UI/AboutForm.cs
+++ Assets/GameMain/Scripts/UI/AboutForm.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace StarForce
@@ -7,20 +6,52 @@ namespace StarForce
     public class AboutForm : UGuiForm
     {
         [SerializeField]
+        private RectTransform m_Transform = null;
+
+        [SerializeField]
+        private float m_DelaySeconds = 1f;
+
+        [SerializeField]
+        private float m_ScrollSpeed = 1f;
+
+        [SerializeField]
+        private float m_ScrollMax = 0f;
+
+        [SerializeField]
+        private float m_ScrollMin = 0f;
+
+        [SerializeField]
         private Text m_TitleText = null;
 
         [SerializeField]
         private Text m_DescriptionText = null;
 
+        [SerializeField]
+        private Text m_WebsiteText = null;
+
+        [SerializeField]
+        private Text m_SummaryText = null;
+
+        [SerializeField]
+        private Text m_IntroductionText = null;
+
+        private float m_ElapseSeconds = 0f;
+
         protected internal override void OnOpen(object userData)
         {
             base.OnOpen(userData);
+
+            m_ElapseSeconds = 0f;
+            m_Transform.SetLocalPositionY(0f);
 
             // 换个音乐
             GameEntry.Sound.PlayMusic(3);
 
             m_TitleText.text = GameEntry.Localization.GetString("Game.Name");
             m_DescriptionText.text = GameEntry.Localization.GetString("Game.Description");
+            m_WebsiteText.text = GameEntry.Localization.GetString("Game.Website");
+            m_SummaryText.text = GameEntry.Localization.GetString("Game.Summary");
+            m_IntroductionText.text = GameEntry.Localization.GetString("Game.Introduction");
         }
 
         protected internal override void OnClose(object userData)
@@ -35,9 +66,14 @@ namespace StarForce
         {
             base.OnUpdate(elapseSeconds, realElapseSeconds);
 
-            if (EventSystem.current.IsPointerOverGameObject(0))
+            m_ElapseSeconds += elapseSeconds;
+            if (m_ElapseSeconds > m_DelaySeconds)
             {
-                Close();
+                m_Transform.AddLocalPositionY(m_ScrollSpeed * elapseSeconds);
+                if (m_Transform.localPosition.y > m_ScrollMax)
+                {
+                    m_Transform.SetLocalPositionY(m_ScrollMin);
+                }
             }
         }
     }
