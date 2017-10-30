b28631cf751ffb3efdc2f4afdd0039fe392e0d6e
Jiang Yin <jiangyin@gameframework.cn>
2017-10-31 01:13:27 +0800

    调整配置文件

---
+++ Assets/GameMain/Scripts/Editor/GameFrameworkConfigs.cs
@@ -0,0 +1,22 @@
+﻿using GameFramework;
+using UnityEngine;
+using UnityGameFramework.Editor;
+using UnityGameFramework.Editor.AssetBundleTools;
+
+namespace StarForce.Editor
+{
+    public static class GameFrameworkConfigs
+    {
+        [BuildSettingsConfigPath]
+        public static string BuildSettingsConfig = Utility.Path.GetCombinePath(Application.dataPath, "GameMain/Configs/BuildSettings.xml");
+
+        [AssetBundleBuilderConfigPath]
+        public static string AssetBundleBuilderConfig = Utility.Path.GetCombinePath(Application.dataPath, "GameMain/Configs/AssetBundleBuilder.xml");
+
+        [AssetBundleEditorConfigPath]
+        public static string AssetBundleEditorConfig = Utility.Path.GetCombinePath(Application.dataPath, "GameMain/Configs/AssetBundleEditor.xml");
+
+        [AssetBundleCollectionConfigPath]
+        public static string AssetBundleCollectionConfig = Utility.Path.GetCombinePath(Application.dataPath, "GameMain/Configs/AssetBundleCollection.xml");
+    }
+}
