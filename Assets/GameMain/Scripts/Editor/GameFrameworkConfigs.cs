d04b1066d5e78a154c27bc3b6a02a6cc5eb8a600
Jiang Yin <jiangyin@gameframework.cn>
2020-04-20 11:44:12 +0800

    更新构建工具

--- Assets/GameMain/Scripts/Editor/GameFrameworkConfigs.cs
+++ Assets/GameMain/Scripts/Editor/GameFrameworkConfigs.cs
@@ -9,7 +9,7 @@ using GameFramework;
 using System.IO;
 using UnityEngine;
 using UnityGameFramework.Editor;
-using UnityGameFramework.Editor.AssetBundleTools;
+using UnityGameFramework.Editor.ResourceTools;
 
 namespace StarForce.Editor
 {
@@ -18,13 +18,13 @@ namespace StarForce.Editor
         [BuildSettingsConfigPath]
         public static string BuildSettingsConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/BuildSettings.xml"));
 
-        [AssetBundleBuilderConfigPath]
-        public static string AssetBundleBuilderConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/AssetBundleBuilder.xml"));
+        [ResourceCollectionConfigPath]
+        public static string ResourceCollectionConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/ResourceCollection.xml"));
+		
+        [ResourceEditorConfigPath]
+        public static string ResourceEditorConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/ResourceEditor.xml"));
 
-        [AssetBundleEditorConfigPath]
-        public static string AssetBundleEditorConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/AssetBundleEditor.xml"));
-
-        [AssetBundleCollectionConfigPath]
-        public static string AssetBundleCollectionConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/AssetBundleCollection.xml"));
+        [ResourceBuilderConfigPath]
+        public static string ResourceBuilderConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/ResourceBuilder.xml"));
     }
 }
